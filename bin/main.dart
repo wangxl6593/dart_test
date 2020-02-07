10c089ab99e0d460b98a8feb264272b8cc2e726f
wangxl <wangxl@neusoft.com>
2020-02-07 19:43:15 +0800

    board display

--- bin/main.dart
+++ bin/main.dart
@@ -1,5 +1,8 @@
 import 'package:dart_test/dart_test.dart' as dart_test;
-
+ 
 void main(List<String> arguments) {
-  print('Hello world: ${dart_test.calculate()}!');
+  var board = dart_test.createEmptyBoard();
+  board[0][1] = 1;
+  board[2][2] = 2;
+  dart_test.displayBoard(board);
 }
--- lib/dart_test.dart
+++ lib/dart_test.dart
@@ -1,3 +1,30 @@
-int calculate() {
-  return 6 * 7;
+import 'dart:io';
+import 'package:ansicolor/ansicolor.dart';
+
+List<List<int>> createEmptyBoard() {
+  return List.generate(10, (_) => List.generate(10, (_) => 0));
+}
+
+void displayBoard(List<List<int>> board) {
+  var pen = AnsiPen()..xterm(11);
+  var pen1 = AnsiPen()..xterm(9);
+  var pen2 = AnsiPen()..xterm(10);
+  var pen3 = AnsiPen()..xterm(15);
+  for (var i = 0; i < board.length; i++) {
+    stdout.write(pen('${9-i}'));
+    for (var j = 0; j < board[0].length; j++) {
+      if (board[i][j] == 1) {
+        stdout.write(pen1('  X'));
+      } else if (board[i][j] == 2) {
+        stdout.write(pen2('  X'));
+      } else {
+        stdout.write(pen3('  O'));
+      }
+    }
+    stdout.write('\n');
+  }
+  stdout.write(' ');
+  for (var i = 0; i < board[0].length; i++){
+    stdout.write(pen('  $i'));
+  }
 }
