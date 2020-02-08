52fdb1a6d016c0392f5977032713496cbeb7ef2f
wangxl <wangxl@neusoft.com>
2020-02-08 20:08:20 +0800

    102

--- bin/main.dart
+++ bin/main.dart
@@ -1,8 +1,48 @@
 import 'package:dart_test/dart_test.dart' as dart_test;
- 
-void main(List<String> arguments) {
+import 'dart:io'; 
+
+int flag = 1;
+
+void main(){
   var board = dart_test.createEmptyBoard();
-  board[0][1] = 1;
-  board[2][2] = 2;
-  dart_test.displayBoard(board);
+  String step;
+  int x, y;
+
+  do  {
+   dart_test.displayBoard(board);
+   if (dart_test.winner(board) == 1){
+      print('1 is winner!');
+      break;
+    }else if(dart_test.winner(board) == 2){
+      print('2 is winner!');
+      break;
+    }
+   stdout.write("$flag turn : ");
+   step = stdin.readLineSync();
+   if (step.isEmpty) {
+     print('game is canced!');
+     break;
+   }
+ try {
+     x = int.parse(step.substring(0,step.indexOf(',')).trim());
+     y = int.parse(step.substring(step.indexOf(',') + 1,step.length).trim());
+  if (step.contains(',') && step.indexOf(',') == step.lastIndexOf(',') && x < board.length && y <board.length ) {
+    if (board[x][board.length-1-y] != 0) {
+      print('In  valid,$flag try again!');
+      continue;
+      }
+    if (flag == 1) {
+      board[x][board.length-1-y] = flag;
+      flag = 2;
+    } else {
+      board[x][board.length-1-y] = flag;
+      flag = 1;  
+    }
+   }
+   } catch(FormatException){
+   print('Invalid,$flag try again!');
+   }
+  } 
+  while (true);
+
 }
--- lib/dart_test.dart
+++ lib/dart_test.dart
@@ -1,6 +1,7 @@
 import 'dart:io';
 import 'package:ansicolor/ansicolor.dart';
 
+
 List<List<int>> createEmptyBoard() {
   return List.generate(10, (_) => List.generate(10, (_) => 0));
 }
@@ -13,9 +14,9 @@ void displayBoard(List<List<int>> board) {
   for (var i = 0; i < board.length; i++) {
     stdout.write(pen('${9-i}'));
     for (var j = 0; j < board[0].length; j++) {
-      if (board[i][j] == 1) {
+      if (board[j][i] == 1) {
         stdout.write(pen1('  X'));
-      } else if (board[i][j] == 2) {
+      } else if (board[j][i] == 2) {
         stdout.write(pen2('  X'));
       } else {
         stdout.write(pen3('  O'));
@@ -27,4 +28,127 @@ void displayBoard(List<List<int>> board) {
   for (var i = 0; i < board[0].length; i++){
     stdout.write(pen('  $i'));
   }
+  stdout.write('\n');
 }
+
+int winner(List<List<int>> board){
+  int i=0,j=0;
+//line1
+  for (var x=0; x<board.length; x++) {
+    for (var y=0; y<board.length; y++){
+      if (board[x][y] == 1) {
+        i +=1;
+        if (i == 5) {
+          return 1;
+          }
+      }else{i=0;}
+      if (board[x][y] == 2){
+        j +=1;
+        if (j ==5){
+          return 2;
+        }
+      }else{j=0;}
+    }
+    i =0;
+    j =0;
+  }
+//line2
+  for (var y=0; y<board.length; y++) {
+    for (var x=0; x<board.length; x++){
+      if (board[x][y] == 1) {
+        i +=1;
+        if (i == 5) {
+          return 1;
+          }
+      }else{i=0;}
+      if (board[x][y] == 2){
+        j +=1;
+        if (j ==5){
+          return 2;
+        }
+      }else{j=0;}
+    }
+    i =0;
+    j =0;
+  }
+//left top triangle 
+  for (var x=4; x<board.length; x++) {
+    for (var y=0; y<=x; y++){
+      if (board[x-y][y] == 1) {
+        i +=1;
+        if (i == 5) {
+          return 1;
+          }
+      }else{i=0;}
+      if (board[x-y][y] == 2){
+        j +=1;
+        if (j ==5){
+          return 2;
+        }
+      }else{j=0;}
+    }
+    i =0;
+    j =0;
+  }
+//right bottom triangle 
+  for (var x=board.length-5; x>0; x--) {
+    for (var y=board.length -1 ; y>=x; y--){
+      if (board[board.length -1 -y +x][y] == 1) {
+         i +=1;
+         if (i == 5) {
+          return 1;
+          }
+      }else{i=0;}
+      if (board[board.length -1 -y +x][y] == 2) {
+        j +=1;
+        if (j ==5){
+          return 2;
+        }
+      }else{j=0;}
+    }
+    i =0;
+    j =0;
+  }
+//left bottom triangle 
+ for (var y=0; y<=board.length-5; y++) {
+    for (var x=0 ; x<=board.length -1 -y; x++){
+      if (board[x][x+y] == 1) {
+        i +=1;
+        if (i == 5) {
+          return 1;
+          }
+      }else{i=0;}
+      if (board[x][x+y] == 2){
+        j +=1;
+        if (j ==5){
+          return 2;
+        }
+      }else{j=0;}
+    }
+    i =0;
+    j =0;
+  }
+//right top triangle
+  for (var x=1; x<=board.length-5; x++) {
+    for (var y=0 ; y<=board.length -1 - x ; y++){
+      if (board[x+y][y] == 1) {
+        i +=1;
+        if (i == 5) {
+          return 1;
+          }
+      }else{i=0;}
+      if (board[x+y][y] == 2){
+        j +=1;
+        if (j ==5){
+          return 2;
+        }
+      }else{j=0;}
+    }
+    i =0;
+    j =0;
+  }
+  return 0;
+}
+
+
+
